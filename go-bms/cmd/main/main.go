a463f4f00266e01d3f93ad0ae38480fc51bfc9dc
Vince <vincivenv@gmail.com>
2022-08-20 17:20:22 +0200

    Model funcs, utils

--- go-bms/cmd/main/main.go
+++ go-bms/cmd/main/main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/mrdvince/gollop/bms/pkg/routes"
+	"log"
+	"net/http"
+)
+
+func main() {
+	router := mux.NewRouter()
+	routes.RegisterBMSRoutes(router)
+	http.Handle("/", router)
+	log.Fatal(http.ListenAndServe(":9010", router))
+}
