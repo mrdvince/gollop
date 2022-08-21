9ffebbf6a666e8e35c7981a9d056c1cedaf7c29c
Vince <vincivenv@gmail.com>
2022-08-21 14:02:32 +0200

    Controller (whatever that is)

--- go-bms/pkg/controllers/bms-controller.go
+++ go-bms/pkg/controllers/bms-controller.go
@@ -1,20 +1,65 @@
 package controllers
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/mrdvince/gollop/bms/pkg/models"
+	"github.com/mrdvince/gollop/bms/pkg/utils"
+	"net/http"
+	"strconv"
+)
 
 func CreateBooks(writer http.ResponseWriter, request *http.Request) {
-
+	CreateBook := &models.Book{}
+	utils.ParseBody(request, CreateBook)
+	b := CreateBook.CreateBook()
+	res, _ := json.Marshal(b)
+	response(writer, res)
 }
 
-func GetBooks(writer http.ResponseWriter, request *http.Request) {
+func response(writer http.ResponseWriter, res []byte) {
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusOK)
+	_, _ = writer.Write(res)
+}
 
+func GetBooks(writer http.ResponseWriter, _ *http.Request) {
+	books := models.GetAllBooks()
+	res, _ := json.Marshal(books)
+	response(writer, res)
 }
 func GetBookByID(writer http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	bookID := vars["ID"]
+	ID, err := strconv.ParseInt(bookID, 0, 0)
+	if err != nil {
+		fmt.Println("Error while parsing book")
+	}
+	bookDetails, _ := models.GetBookById(ID)
+	res, _ := json.Marshal(bookDetails)
+	response(writer, res)
 
 }
 func DeleteBook(writer http.ResponseWriter, request *http.Request) {
-
+	vars := mux.Vars(request)
+	bookID := vars["ID"]
+	ID, err := strconv.ParseInt(bookID, 0, 0)
+	if err != nil {
+		fmt.Println("Error parsing book")
+	}
+	book := models.DeleteBook(ID)
+	res, _ := json.Marshal(book)
+	response(writer, res)
 }
 func UpdateBook(writer http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	bookID := vars["ID"]
+	ID, err := strconv.ParseInt(bookID, 0, 0)
+	if err != nil {
+		fmt.Println("Error parsing book")
+	}
+	models.DeleteBook(ID)
+	CreateBooks(writer, request)
 
 }
